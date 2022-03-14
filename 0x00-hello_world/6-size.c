815519ac0ab14bc7d0cb95cb0a21a60749257c16
Opiyo65 <apenosams@gmail.com>
2022-03-14 05:28:52 -0700

    changes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,5 +19,6 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
 	printf("size of a float: %zu byte(s)\n", sizeof(floatType));
+
 	return (0);
 }
