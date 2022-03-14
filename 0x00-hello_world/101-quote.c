451dfb4af0f1819980e49d2de7fcf22eef90bc78
Opiyo65 <apenosams@gmail.com>
2022-03-14 07:04:34 -0700

    changes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - program that print 'and that piece of art is useful" - Dora Korpar, 2015-10-19
+ *
+ * Return: always 1 (succes)
+ */
+int main(void)
+{
+	puts("and that piece of art is useful \" - Dora Korpar, 2015-10-19");
+
+	return (1);
+}
