56f7f0ad124dc2dacd37975152d26dc5ab26df70
Opiyo65 <apenosams@gmail.com>
2022-03-25 22:12:26 -0700

    changes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
 #include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- *Return: on success 1.
- *on error, -1 is returned, and errno is set approprietelly.
- */
-int ft_putchar(char c)
+ * * main - prints a string
+ * * Description: the string is "_putchar"
+ * *
+ * * Return: 0
+ **/
+int main(void)
 {
-	return (write(1, &c, 1));
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: always 0 (success)
+ **/
+void print_alphabet_x10(void)
+{
+	int count;
+	int y;
+	count = 0;
+	while (count < 10)
+	{
+		for (y = 97; y < 122; y++)
+			_putchar(y);
+		_putchar(\n);
+	}
+}
